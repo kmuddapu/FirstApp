82b3ab11d272419e3b7c942a6bb8c5257f025800
Kalyan Muddapu <kalyan.muddapu@penske.com>
2021-07-30 15:05:40 -0400

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kalyan Muddapu on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
